1332b004e031931cc08e3089464842f2efa7f513
Felix Geisendörfer <felix@felixge.de>
2021-02-04 14:30:29 +0100

    split workloads

--- bench/workload_mutex.go
+++ bench/workload_mutex.go
@@ -5,33 +5,6 @@ import (
 	"sync"
 )
 
-func chanWorkload(goroutines, ops, depth, bufsize int) error {
-	if goroutines%2 != 0 {
-		return fmt.Errorf("bad goroutines: %d: must be a multiple of 2", goroutines)
-	}
-
-	wg := &sync.WaitGroup{}
-	for j := 0; j < goroutines/2; j++ {
-		ch := make(chan struct{}, bufsize)
-		wg.Add(1)
-		go atStackDepth(depth, func() {
-			defer wg.Done()
-			for i := 0; i < ops; i++ {
-				ch <- struct{}{}
-			}
-		})
-		wg.Add(1)
-		go atStackDepth(depth, func() {
-			defer wg.Done()
-			for i := 0; i < ops; i++ {
-				<-ch
-			}
-		})
-	}
-	wg.Wait()
-	return nil
-}
-
 func mutexWorkload(goroutines, ops, depth int) error {
 	if goroutines%2 != 0 {
 		return fmt.Errorf("bad goroutines: %d: must be a multiple of 2", goroutines)
